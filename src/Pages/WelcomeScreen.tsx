be87a15c9dd7226c17b8223db5e2334dd76706c6
SlySL <91503617+Sly-SL@users.noreply.github.com>
2025-11-03 18:58:19 +0100

    fast

--- src/Pages/WelcomeScreen.tsx
+++ src/Pages/WelcomeScreen.tsx
@@ -133,21 +133,21 @@ const WelcomeScreen: React.FC<WelcomeScreenProps> = ({ onLoadingComplete }) => {
                                 <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold space-y-2 sm:space-y-4">
                                     <div className="mb-2 sm:mb-4">
                     <span data-aos="fade-right" data-aos-delay="200" className="inline-block px-2 bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
-                        Test
+                        Welcome
                     </span>{' '}
                                         <span data-aos="fade-right" data-aos-delay="400" className="inline-block px-2 bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
-                      Ho
+                                            to
                     </span>{' '}
                                         <span data-aos="fade-right" data-aos-delay="600" className="inline-block px-2 bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
-                      st
+                      my
                     </span>
                                     </div>
                                     <div>
                     <span data-aos="fade-up" data-aos-delay="800" className="inline-block px-2 bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
-                      na
+                      Portfolio
                     </span>{' '}
                                         <span data-aos="fade-up" data-aos-delay="1000" className="inline-block px-2 bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
-                      Vercel
+                      Website
                     </span>
                                     </div>
                                 </h1>
